325faef90c7701d03ca6be909eb9672e64b85621
Chau Tran <nartc7789@gmail.com>
2023-09-18 10:43:01 -0500

    fix(map-array): add index to mapFn

--- libs/ngxtension/map-array/src/map-array.spec.ts
+++ libs/ngxtension/map-array/src/map-array.spec.ts
@@ -1,14 +1,16 @@
-import { of } from 'rxjs';
+import { combineLatest, of } from 'rxjs';
 import { mapArray } from './map-array';
 
 describe(mapArray.name, () => {
 	const dummyObs = of([1, 2, 3]);
 
-	it('given an observable of [1,2,3] and a mapping function of adding 1 to each element, then result is an observable of [2,3,4]', (done) => {
+	it('given an observable of [1,2,3] and a mapping function of adding 1 to each element, then result is an observable of [2,3,4], [2, 4, 6] with index', (done) => {
 		const result = dummyObs.pipe(mapArray((n) => n + 1));
+		const resultWithIndex = dummyObs.pipe(mapArray((n, i) => n + 1 + i));
 
-		result.subscribe((r) => {
+		combineLatest([result, resultWithIndex]).subscribe(([r, rI]) => {
 			expect(r).toEqual([2, 3, 4]);
+			expect(rI).toEqual([2, 4, 6]);
 			done();
 		});
 	});
--- libs/ngxtension/map-array/src/map-array.ts
+++ libs/ngxtension/map-array/src/map-array.ts
@@ -1,5 +1,4 @@
 import { map } from 'rxjs';
 
-export const mapArray = <T, R>(mapFn: (item: T) => R) => {
-	return map((array: T[]) => array.map((item) => mapFn(item)));
-};
+export const mapArray = <T, R>(mapFn: (item: T, index: number) => R) =>
+	map((array: T[]) => array.map((item, index) => mapFn(item, index)));
