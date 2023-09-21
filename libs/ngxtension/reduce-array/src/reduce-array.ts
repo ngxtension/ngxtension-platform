8f3af9f494f66be694e0f9871350d33de8a10288
tomer953 <tomer953@gmail.com>
2023-09-21 10:31:02 +0300

    update reduce-array: adding type overloads

--- libs/ngxtension/reduce-array/src/reduce-array.ts
+++ libs/ngxtension/reduce-array/src/reduce-array.ts
@@ -1,10 +1,19 @@
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+export function reduceArray<T>(
+	reduceFn: (acc: T, item: T, index: number) => T
+): (source: Observable<T[]>) => Observable<T>;
+
 export function reduceArray<T, R = T>(
 	reduceFn: (acc: R, item: T, index: number) => R,
 	initialValue?: R
-): (source: Observable<T[]>) => Observable<R> {
+): (source: Observable<T[]>) => Observable<R>;
+
+export function reduceArray<T>(
+	reduceFn: (acc: any, item: T, index: number) => any,
+	initialValue?: any
+): (source: Observable<T[]>) => Observable<any> {
 	return map((array: T[]) => {
 		// call reduce function with initialValue
 		if (initialValue !== undefined) {
@@ -18,7 +27,7 @@ export function reduceArray<T, R = T>(
 				return undefined;
 			}
 			// if array is not empty, call the reduceFn without initialValue
-			return (array as any).reduce(reduceFn);
+			return array.reduce(reduceFn);
 		}
 	});
 }
