fe702ad5afb0ab2be0664feb447db64e96baa7a0
nevzatopcu <nevzatopcu@gmail.com>
2023-10-14 11:00:38 +0300

    feat(activeElement): use assertInjector

--- libs/ngxtension/active-element/src/active-element.ts
+++ libs/ngxtension/active-element/src/active-element.ts
@@ -1,20 +1,22 @@
 import { DOCUMENT } from '@angular/common';
-import { assertInInjectionContext, inject, type Injector } from '@angular/core';
+import { inject, Injector } from '@angular/core';
+import { assertInjector } from 'ngxtension/assert-injector';
 import { fromEvent, map, merge, shareReplay } from 'rxjs';
 
 export function injectActiveElement(injector?: Injector) {
-	injector ?? assertInInjectionContext(injectActiveElement);
-	const doc = injector ? injector.get(DOCUMENT) : inject(DOCUMENT);
-
-	return merge(
-		fromEvent(doc, 'focus', { capture: true, passive: true }).pipe(
-			map(() => true)
-		),
-		fromEvent(doc, 'blur', { capture: true, passive: true }).pipe(
-			map(() => false)
-		)
-	).pipe(
-		map((hasFocus) => (hasFocus ? doc.activeElement : null)),
-		shareReplay({ refCount: true, bufferSize: 1 })
-	);
+	return assertInjector(injectActiveElement, injector, () => {
+		const assertedInjector = inject(Injector);
+		const doc = assertedInjector.get(DOCUMENT);
+		return merge(
+			fromEvent(doc, 'focus', { capture: true, passive: true }).pipe(
+				map(() => true)
+			),
+			fromEvent(doc, 'blur', { capture: true, passive: true }).pipe(
+				map(() => false)
+			)
+		).pipe(
+			map((hasFocus) => (hasFocus ? doc.activeElement : null)),
+			shareReplay({ refCount: true, bufferSize: 1 })
+		);
+	});
 }
