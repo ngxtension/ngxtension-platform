48a5f807bacf910264c5fbc78e183480f25764be
Chau Tran <nartc7789@gmail.com>
2023-11-09 16:51:28 -0600

    chore: upgrade packages

--- libs/ngxtension/auto-effect/src/auto-effect.spec.ts
+++ libs/ngxtension/auto-effect/src/auto-effect.spec.ts
@@ -42,28 +42,28 @@ describe(injectAutoEffect.name, () => {
 
 	it('should work properly in afterNextRender (OnInit)', () => {
 		const [fixture, [tick, cleanUpTick], setCount] = setup();
-		expect(tick()).toEqual(0);
-		expect(cleanUpTick()).toEqual(0);
-
-		setCount(2);
 		expect(tick()).toEqual(1);
 		expect(cleanUpTick()).toEqual(0);
 
 		setCount(2);
-		expect(tick()).toEqual(1);
-		expect(cleanUpTick()).toEqual(0);
-
-		setCount(3);
 		expect(tick()).toEqual(2);
 		expect(cleanUpTick()).toEqual(1);
 
-		fixture.destroy();
+		setCount(2);
 		expect(tick()).toEqual(2);
+		expect(cleanUpTick()).toEqual(1);
+
+		setCount(3);
+		expect(tick()).toEqual(3);
 		expect(cleanUpTick()).toEqual(2);
+
+		fixture.destroy();
+		expect(tick()).toEqual(3);
+		expect(cleanUpTick()).toEqual(3);
 
 		setCount(4);
 		setCount(5);
-		expect(tick()).toEqual(2);
-		expect(cleanUpTick()).toEqual(2);
+		expect(tick()).toEqual(3);
+		expect(cleanUpTick()).toEqual(3);
 	});
 });
