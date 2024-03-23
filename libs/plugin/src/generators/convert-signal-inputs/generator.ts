09f075db2b99f221232bde7acb8efec8d2de0333
Josh Morony <joshua.morony@gmail.com>
2024-03-24 10:02:08 +1030

    fix(convert-signal-inputs): handle automatic semicolon insertion issues (#294)
    * fix(convert-signal-inputs): fail gracefully for automatic semicolon insertion issues
    
    * fix(convert-signal-inputs): throw if parsing failes due to ASI

--- libs/plugin/src/generators/convert-signal-inputs/generator.ts
+++ libs/plugin/src/generators/convert-signal-inputs/generator.ts
@@ -307,8 +307,16 @@ export async function convertSignalInputsGenerator(
 
 						node.replaceWithText(newProperty.print());
 
-						// remove old class property Input
-						newProperty.remove();
+						// fail gracefully for nodes not terminated with a semi-colon
+						try {
+							// remove old class property Input
+							newProperty.remove();
+						} catch (err) {
+							logger.warn(
+								`[ngxtension] "${path}" Failed to parse node, check that declarations are terminated with a semicolon and try again`,
+							);
+							throw err;
+						}
 
 						// track converted inputs
 						convertedInputs.add(name);
