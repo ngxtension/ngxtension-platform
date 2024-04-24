81ff2d7d69c458839174f1f4fc910e4dbe4f345e
Enea Jahollari <jahollarienea14@gmail.com>
2024-04-24 10:02:16 +0200

    fix: reintroduce computedAsync which reexports and shows a deprecation notice

---
+++ libs/ngxtension/computed-async/src/index.ts
@@ -0,0 +1,11 @@
+import { derivedAsync } from 'ngxtension/derived-async';
+
+/**
+ * This function is deprecated. Use `derivedAsync` instead.
+ * Run: `npx ng update ngxtension@latest` to migrate automatically.
+ *
+ * @deprecated Use `derivedAsync` instead. Will be removed in v3.
+ */
+const computedAsync = derivedAsync;
+
+export { computedAsync };
