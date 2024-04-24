3de9e093a421ae4efccf9c86d6c8b08b3e0292e8
Enea Jahollari <jahollarienea14@gmail.com>
2024-04-24 14:43:14 +0200

    fix: reintroduce computedAsync which reexports and shows a deprecation notice (#342)

---
+++ libs/ngxtension/computed-from/src/index.ts
@@ -0,0 +1,11 @@
+import { derivedFrom } from 'ngxtension/derived-from';
+
+/**
+ * This function is deprecated. Use `derivedFrom` instead.
+ * Run: `npx ng update ngxtension@latest` to migrate automatically.
+ *
+ * @deprecated Use `derivedFrom` instead. Will be removed in v3.
+ */
+const computedFrom = derivedFrom;
+
+export { computedFrom };
