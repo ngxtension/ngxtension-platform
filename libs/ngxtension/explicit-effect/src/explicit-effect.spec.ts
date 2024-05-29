2d6e2e9b3fbd9d11512583fd8e74fb1d5ff238f7
Matthieu Riegler <kyro38@gmail.com>
2024-05-29 22:20:11 +0200

    refactor: add unit tests to `explicitEffect()`

--- libs/ngxtension/explicit-effect/src/explicit-effect.spec.ts
+++ libs/ngxtension/explicit-effect/src/explicit-effect.spec.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { explicitEffect } from './explicit-effect';
 
 describe(explicitEffect.name, () => {
 	let log: string[] = [];
+	let cleanupLog: string[] = [];
 
 	beforeEach(() => {
 		log = [];
+		cleanupLog = [];
 	});
 
 	@Component({
@@ -16,22 +18,19 @@ describe(explicitEffect.name, () => {
 	class Foo {
 		count = signal(0);
 		state = signal('idle');
+		foobar = signal<'foo' | 'bar'>('foo');
 
-		constructor() {
-			const dep1 = signal(0);
+		eff = explicitEffect(
+			[this.count, this.state],
+			([count, state], cleanUpFn) => {
+				this.foobar();
+				log.push(`count updated ${count}, ${state}`);
 
-			const dep = () => {
-				dep1.update((x) => x + 1); // This will trigger the effect error
-				return dep1();
-			};
-
-			explicitEffect(
-				[this.count, this.state, dep],
-				([count, state, dep], cleanup) => {
-					log.push(`count updated ${count}, ${state}`);
-				},
-			);
-		}
+				cleanUpFn(() => {
+					cleanupLog.push('cleanup');
+				});
+			},
+		);
 	}
 
 	it('should register deps and run effect', () => {
@@ -49,8 +48,62 @@ describe(explicitEffect.name, () => {
 		fixture.detectChanges();
 		expect(log.length).toBe(1);
 
-		fixture.componentInstance.state.set('pending');
+		fixture.componentInstance.foobar.set('foo');
 		fixture.detectChanges();
 		expect(log.length).toBe(1);
+	});
+
+	it('should run the effect cleanupFn', () => {
+		const fixture = TestBed.createComponent(Foo);
+		expect(log.length).toBe(0);
+		fixture.detectChanges();
+		expect(log.length).toBe(1);
+		expect(cleanupLog.length).toBe(0);
+
+		fixture.componentInstance.count.set(1);
+		TestBed.flushEffects();
+		expect(log.length).toBe(2);
+		expect(cleanupLog.length).toBe(1);
+	});
+
+	it('should accept computed, functions etc.', () => {
+		const log: string[] = [];
+		const count = signal(0);
+		const state = signal('idle');
+		const doubleCount = computed(() => count() * 2);
+		const foobar = signal<'foo' | 'bar'>('foo');
+		const result = () => count() + doubleCount() + foobar();
+
+		TestBed.runInInjectionContext(() => {
+			explicitEffect(
+				[count, state, doubleCount, result],
+				([count, state, doubleCount, result]) => {
+					log.push(
+						`count updated ${count}, ${state}, ${doubleCount}, ${result}`,
+					);
+				},
+			);
+			expect(log.length).toBe(0);
+			TestBed.flushEffects();
+			expect(log.length).toBe(1);
+
+			foobar.set('bar');
+			TestBed.flushEffects();
+			expect(log.length).toBe(2);
+			expect(log.some((v) => v.includes('bar'))).toBeTruthy();
+		});
+	});
+
+	it('should pass the right types to the effect callback', () => {
+		const count = signal(0);
+		const state = signal('idle');
+
+		TestBed.runInInjectionContext(() => {
+			explicitEffect([count, state], ([count, state]) => {
+				const _count: number = count;
+				const _state: string = state;
+				console.log(_count, _state);
+			});
+		});
 	});
 });
