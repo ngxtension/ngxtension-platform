2d26b7a6bf60675508dc51798333175985a4be14
Trong Nguyen <nguyenphutrong.dev@gmail.com>
2024-06-06 23:12:56 +0700

    feat(to-observable-signal): refactor toObservableSignal to handle Observables and Subscribables
    The refactor allows toObservableSignal to handle Observables and Subscribables, expanding its flexibility and usage. It introduces the ability to convert an Observable or Subscribable to a Signal & Observable, enhancing its adaptability. This change aims to improve the overall functionality of toObservableSignal by broadening its capabilities.

--- libs/ngxtension/to-observable-signal/src/to-observable-signal.ts
+++ libs/ngxtension/to-observable-signal/src/to-observable-signal.ts
@@ -6,30 +6,40 @@ import {
 } from '@angular/core';
 import {
 	toObservable,
+	toSignal,
 	type ToObservableOptions,
 } from '@angular/core/rxjs-interop';
-import type { Observable } from 'rxjs';
+import type { Observable, Subscribable } from 'rxjs';
 
 export type ObservableSignal<T> = Signal<T> & Observable<T>;
 
 export function toObservableSignal<T>(
-	s: WritableSignal<T>,
+	s: WritableSignal<T> | Observable<T> | Subscribable<T>,
 	options?: ToObservableOptions,
 ): WritableSignal<T> & Observable<T>;
 export function toObservableSignal<T>(
-	s: Signal<T>,
+	s: Signal<T> | Observable<T> | Subscribable<T>,
 	options?: ToObservableOptions,
 ): ObservableSignal<T>;
 
 export function toObservableSignal<T>(
-	s: Signal<T>,
+	source: Signal<T> | Observable<T> | Subscribable<T>,
 	options?: ToObservableOptions,
 ) {
 	if (isDevMode() && !options?.injector) {
 		assertInInjectionContext(toObservableSignal);
 	}
 
-	const obs = toObservable(s, options);
+	let s: Signal<T | undefined>;
+	let obs: Observable<T>;
+
+	if (typeof source === 'function') {
+		s = source as Signal<T>;
+		obs = toObservable(source as Signal<T>, options);
+	} else {
+		obs = source as unknown as Observable<T>;
+		s = toSignal(obs, { injector: options?.injector });
+	}
 
 	return new Proxy(s, {
 		get(_, prop) {
