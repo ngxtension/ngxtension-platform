c0cd8bc2666bba2aeef534d8d45c08939e08fe3e
Fabien Dehopré <fabien@dehopre.com>
2024-06-11 22:02:47 +0200

    test(when-document-visible): fix tests

--- libs/ngxtension/when-document-visible/src/when-document-visible.spec.ts
+++ libs/ngxtension/when-document-visible/src/when-document-visible.spec.ts
@@ -1,5 +1,6 @@
-import { fakeAsync, tick } from '@angular/core/testing';
-import { timer } from 'rxjs';
+import { Injector } from '@angular/core';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Observable, timer } from 'rxjs';
 import { whenDocumentVisible } from './when-document-visible';
 
 describe(whenDocumentVisible.name, () => {
@@ -16,19 +17,45 @@ describe(whenDocumentVisible.name, () => {
 		document.dispatchEvent(event);
 	}
 
-	it('should emit values only when document is visible', fakeAsync(() => {
-		const source = timer(0, 1000);
-		const callback = jest.fn();
-		triggerVisibilityChange('visible');
-		source.pipe(whenDocumentVisible()).subscribe(callback);
-		expect(callback).toHaveBeenCalledTimes(1);
-		tick(1000);
-		expect(callback).toHaveBeenCalledTimes(2);
-		triggerVisibilityChange('hidden');
-		tick(1000);
-		expect(callback).toHaveBeenCalledTimes(2);
-		triggerVisibilityChange('visible');
-		tick(1000);
-		expect(callback).toHaveBeenCalledTimes(3);
-	}));
+	describe('should emit values only when document is visible', () => {
+		const runTest = (source: Observable<number>): void => {
+			const callback = jest.fn();
+			triggerVisibilityChange('visible');
+			const sub = source.subscribe(callback);
+			tick(); // trigger initial emission
+			expect(callback).toHaveBeenCalledTimes(1);
+			tick(10_000); // will emit 10 times in 10 seconds
+			expect(callback).toHaveBeenCalledTimes(11);
+			triggerVisibilityChange('hidden');
+			tick(10_000);
+			expect(callback).toHaveBeenCalledTimes(11);
+			triggerVisibilityChange('visible');
+			tick(10_000); // will emit 11 times (once initially and 10 times in 10 seconds)
+			expect(callback).toHaveBeenCalledTimes(22);
+			sub.unsubscribe();
+		};
+
+		it('should emit values only when document is visible using provided injector', fakeAsync(() => {
+			const source = timer(0, 1000);
+			runTest(
+				source.pipe(
+					whenDocumentVisible({ injector: TestBed.inject(Injector) }),
+				),
+			);
+		}));
+
+		it('should emit values only when document is visible in injector context', fakeAsync(() => {
+			TestBed.runInInjectionContext(() => {
+				const source = timer(0, 1000);
+				runTest(source.pipe(whenDocumentVisible()));
+			});
+		}));
+
+		it('should emit values only when document is visible using provided document', fakeAsync(() => {
+			TestBed.runInInjectionContext(() => {
+				const source = timer(0, 1000);
+				runTest(source.pipe(whenDocumentVisible({ document })));
+			});
+		}));
+	});
 });
