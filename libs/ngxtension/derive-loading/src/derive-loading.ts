81828f02bc44874e8da3ccd678ac5c4ce42ad03a
Michael Berger <m.gonser@icloud.com>
2024-07-03 16:27:50 +0200

    feat: add deriveLoading RxJs-Operator

---
+++ libs/ngxtension/derive-loading/src/derive-loading.ts
@@ -0,0 +1,69 @@
+import {
+	combineLatest,
+	debounce,
+	distinctUntilChanged,
+	map,
+	merge,
+	MonoTypeOperatorFunction,
+	Observable,
+	OperatorFunction,
+	ReplaySubject,
+	share,
+	startWith,
+	takeUntil,
+	timer,
+} from 'rxjs';
+
+export type DeriveLoadingOptions = {
+	threshold?: number;
+	loadingTime?: number;
+};
+
+export function deriveLoading<T>(
+	options?: DeriveLoadingOptions,
+): OperatorFunction<T, boolean> {
+	const threshold = options?.threshold ?? 500;
+	const loadingTime = options?.loadingTime ?? 1000;
+
+	return function <T>(source: Observable<T>): Observable<boolean> {
+		const result$ = source.pipe(
+			share({
+				connector: () => new ReplaySubject(1),
+				resetOnComplete: false,
+				resetOnRefCountZero: true,
+				resetOnError: true,
+			}),
+		);
+
+		return merge(
+			timer(threshold).pipe(
+				map(() => true),
+				takeUntil(result$),
+			),
+			combineLatest([result$, timer(threshold + loadingTime)]).pipe(
+				map(() => false),
+			),
+		).pipe(startWith(false), distinctUntilChanged(), handleSyncValue());
+	};
+}
+
+function handleSyncValue<T>(): MonoTypeOperatorFunction<any> {
+	return (source$: Observable<T>): Observable<T> => {
+		return new Observable<T>((observer) => {
+			const isReadySubject = new ReplaySubject<unknown>(1);
+
+			const subscription = source$
+				.pipe(
+					/* Wait for all synchronous processing to be done. */
+					debounce(() => isReadySubject),
+				)
+				.subscribe(observer);
+
+			/* Sync emitted values have been processed now.
+			 * Mark source as ready and emit last computed state. */
+			isReadySubject.next(undefined);
+
+			return () => subscription.unsubscribe();
+		});
+	};
+}
