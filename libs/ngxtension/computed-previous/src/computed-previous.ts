da23dfc7f01ca1a2840fb72afafb20ad259001cf
Enea Jahollari <jahollarienea14@gmail.com>
2024-07-15 15:36:22 +0200

    fix(computed-previous): Don't return null for initial value, but return the current value of the source signal instead (#281)

--- libs/ngxtension/computed-previous/src/computed-previous.ts
+++ libs/ngxtension/computed-previous/src/computed-previous.ts
@@ -1,8 +1,8 @@
-import { computed, type Signal } from '@angular/core';
+import { computed, untracked, type Signal } from '@angular/core';
 
 /**
  * Returns a signal that emits the previous value of the given signal.
- * The first time the signal is emitted, the previous value will be `null`.
+ * The first time the signal is emitted, the previous value will be the same as the current value.
  *
  * @example
  * ```ts
@@ -16,28 +16,32 @@ import { computed, type Signal } from '@angular/core';
  *
  * Logs:
  * // Current value: 0
- * // Previous value: null
+ * // Previous value: 0
  *
  * value.set(1);
  *
  * Logs:
  * // Current value: 1
  * // Previous value: 0
+ *
+ * value.set(2);
+ *
+ * Logs:
+ * // Current value: 2
+ * // Previous value: 1
  *```
-
+ *
  * @param s Signal to compute previous value for
  * @returns Signal that emits previous value of `s`
  */
-export function computedPrevious<T>(s: Signal<T>): Signal<T | null> {
+export function computedPrevious<T>(s: Signal<T>): Signal<T> {
 	let current = null as T;
-	let previous = null as T;
+	let previous = untracked(() => s()); // initial value is the current value
 
 	return computed(() => {
-		const value = s();
-		if (value !== current) {
-			previous = current;
-			current = value;
-		}
-		return previous;
+		current = s();
+		const result = previous;
+		previous = current;
+		return result;
 	});
 }
