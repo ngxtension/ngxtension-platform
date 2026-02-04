32dc2fa98e9e574f2172b7dbc14b4a75f8de0d22
Andreas Dorner <andreas1dorner@gmail.com>
2026-02-04 19:58:56 +0100

    feat(create-notifier): add support for dependencies

--- libs/ngxtension/create-notifier/src/create-notifier.spec.ts
+++ libs/ngxtension/create-notifier/src/create-notifier.spec.ts
@@ -1,4 +1,4 @@
-import { effect } from '@angular/core';
+import { effect, signal } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { createNotifier } from './create-notifier';
 
@@ -61,6 +61,97 @@ describe(createNotifier.name, () => {
 			trigger.notify();
 			TestBed.flushEffects();
 			expect(testFn).toHaveBeenCalledTimes(2);
+		});
+	});
+
+	it('should work with options.deps', () => {
+		TestBed.runInInjectionContext(() => {
+			let notifyValue: number;
+			const dep1 = signal<any>(null);
+			const dep2 = signal<any>(null);
+			const testFn = jest.fn();
+			const trigger = createNotifier({ deps: [dep1, dep2] });
+
+			effect(() => {
+				notifyValue = trigger.listen();
+				testFn();
+			});
+
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(1);
+			expect(notifyValue!).toBe(1);
+
+			trigger.notify();
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(2);
+			expect(notifyValue!).toBe(2);
+
+			dep1.set(1);
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(3);
+			expect(notifyValue!).toBe(3);
+
+			dep1.set(2);
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(4);
+			expect(notifyValue!).toBe(4);
+
+			dep1.set(2);
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(4);
+			expect(notifyValue!).toBe(4);
+
+			trigger.notify();
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(5);
+			expect(notifyValue!).toBe(5);
+		});
+	});
+
+	it('should work with options.deps and options.depsEmitInitially=false', () => {
+		TestBed.runInInjectionContext(() => {
+			let notifyValue: number;
+			const dep1 = signal<any>(null);
+			const dep2 = signal<any>(null);
+			const testFn = jest.fn();
+			const trigger = createNotifier({
+				deps: [dep1, dep2],
+				depsEmitInitially: false,
+			});
+
+			effect(() => {
+				notifyValue = trigger.listen();
+				testFn();
+			});
+
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(1);
+			expect(notifyValue!).toBe(0);
+
+			trigger.notify();
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(2);
+			expect(notifyValue!).toBe(1);
+
+			dep1.set(1);
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(3);
+			expect(notifyValue!).toBe(2);
+
+			dep1.set(2);
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(4);
+			expect(notifyValue!).toBe(3);
+
+			dep1.set(2);
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(4);
+			expect(notifyValue!).toBe(3);
+
+			trigger.notify();
+			TestBed.flushEffects();
+			expect(testFn).toHaveBeenCalledTimes(5);
+			expect(notifyValue!).toBe(4);
 		});
 	});
 });
