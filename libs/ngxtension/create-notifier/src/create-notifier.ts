770da8175e21c8b9782a0f8c46df8444f8b6def4
Andreas Dorner <andreas1dorner@gmail.com>
2026-02-08 18:15:49 +0100

    feat(create-notifier): simplify signal creation based on dependencies

--- libs/ngxtension/create-notifier/src/create-notifier.ts
+++ libs/ngxtension/create-notifier/src/create-notifier.ts
@@ -1,4 +1,4 @@
-import { linkedSignal, Signal } from '@angular/core';
+import { linkedSignal, signal, Signal } from '@angular/core';
 
 type CreateNotifierOptions = Record<string, any> & {
 	deps: Signal<any>[];
@@ -21,23 +21,26 @@ export function createNotifier(options?: CreateNotifierOptions) {
 		...options,
 	};
 
-	const sourceSignal = linkedSignal<number, number>({
-		source: () => {
-			options?.deps?.forEach((dep) => dep()); // Track all dependencies
+	// without explicit deps we can simplify to a simple signal
+	const sourceSignal = !options.deps.length
+		? signal(0)
+		: linkedSignal<number, number>({
+				source: () => {
+					options.deps?.forEach((dep) => dep()); // Track all dependencies
 
-			// - when deps exist, the notifier should start at 1, because it immediately emits.
-			//  -without any deps, it is only based on increments. and those should start at 0.
-			return options?.deps?.length && options?.depsEmitInitially ? 1 : 0;
-		},
-		// Return a new value each time source runs. This ensures deps changes also increment the counter
-		computation: (currentIncrementer, previousValue) => {
-			// Increment from previous value when deps change
-			return previousValue !== undefined
-				? previousValue.value + 1
-				: currentIncrementer;
-		},
-		equal: () => false, // Always notify downstream consumers
-	});
+					// - when deps exist, the notifier should start at 1, because it immediately emits.
+					//  -without any deps, it is only based on increments. and those should start at 0.
+					return options.deps?.length && options.depsEmitInitially ? 1 : 0;
+				},
+				// Return a new value each time source runs. This ensures deps changes also increment the counter
+				computation: (currentIncrementer, previousValue) => {
+					// Increment from previous value when deps change
+					return previousValue !== undefined
+						? previousValue.value + 1
+						: currentIncrementer;
+				},
+				equal: () => false, // Always notify downstream consumers
+			});
 
 	return {
 		notify: () => sourceSignal.update((v) => (v >>> 0) + 1),
