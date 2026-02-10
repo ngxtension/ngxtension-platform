80378f720ddabace839349e5c3f22fd275660fe4
Andreas Dorner <andreas1dorner@gmail.com>
2026-02-10 07:33:37 +0100

    feat(tap-once): enhance tapOnce and tapOnceOnFirstTruthy to ensure unique state per subscription

--- libs/ngxtension/tapOnce/src/tap-once.ts
+++ libs/ngxtension/tapOnce/src/tap-once.ts
@@ -1,28 +1,33 @@
-import { MonoTypeOperatorFunction, concatMap, of, type Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { defer, MonoTypeOperatorFunction, Observable, tap } from 'rxjs';
 
 /**
  * Executes the provided function only once when the first truthy value is emitted.
+ * Uses `defer` to ensure state is unique per subscription.
+ *
  * @param tapFn - Function to execute on the first truthy value.
  * @returns MonoTypeOperatorFunction
  */
 export function tapOnceOnFirstTruthy<T>(
 	tapFn: (t: T) => void,
 ): MonoTypeOperatorFunction<T> {
-	let firstTruthy = true;
 	return (source$: Observable<T>) =>
-		source$.pipe(
-			tap((value) => {
-				if (firstTruthy && !!value) {
-					tapFn(value);
-					firstTruthy = false;
-				}
-			}),
-		);
+		defer(() => {
+			let firstTruthy = true;
+			return source$.pipe(
+				tap((value) => {
+					if (firstTruthy && !!value) {
+						tapFn(value);
+						firstTruthy = false;
+					}
+				}),
+			);
+		});
 }
 
 /**
  * Executes the provided function only once when the value at the specified index is emitted.
+ * Uses `defer` to track index without the overhead of `concatMap`.
+ *
  * @param tapFn - Function to execute on the value at the specified index.
  * @param tapIndex - Index at which to execute the function (default is 0).
  * @returns MonoTypeOperatorFunction
@@ -34,13 +39,17 @@ export function tapOnce<T>(
 	if (tapIndex < 0) {
 		throw new Error('tapIndex must be a non-negative integer');
 	}
+
 	return (source$: Observable<T>) =>
-		source$.pipe(
-			concatMap((value, index) => {
-				if (index === tapIndex) {
-					tapFn(value);
-				}
-				return of(value);
-			}),
-		);
+		defer(() => {
+			let currentIndex = 0;
+			return source$.pipe(
+				tap((value) => {
+					if (currentIndex === tapIndex) {
+						tapFn(value);
+					}
+					currentIndex++;
+				}),
+			);
+		});
 }
