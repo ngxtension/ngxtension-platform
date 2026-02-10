f12e1f156440ffbe12a4285f98af90cadaf42154
Andreas Dorner <andreas1dorner@gmail.com>
2026-02-10 07:33:37 +0100

    feat(tap-once): enhance tapOnce and tapOnceOnFirstTruthy to ensure unique state per subscription

--- libs/ngxtension/tapOnce/src/tap-once.spec.ts
+++ libs/ngxtension/tapOnce/src/tap-once.spec.ts
@@ -2,19 +2,6 @@ import { from, toArray } from 'rxjs';
 import { tapOnce, tapOnceOnFirstTruthy } from './tap-once';
 
 describe(tapOnce.name, () => {
-	it('should execute the function only once at the specified index', (done) => {
-		const tapFn = jest.fn();
-		const in$ = from([1, 2, 3, 4, 5]);
-		const out$ = in$.pipe(tapOnce(tapFn, 2));
-
-		out$.pipe(toArray()).subscribe((r) => {
-			expect(r).toEqual([1, 2, 3, 4, 5]);
-			expect(tapFn).toHaveBeenCalledTimes(1);
-			expect(tapFn).toHaveBeenCalledWith(3);
-			done();
-		});
-	});
-
 	it('should execute the function only once at the default index 0', (done) => {
 		const tapFn = jest.fn();
 		const in$ = from([1, 2, 3, 4, 5]);
@@ -28,10 +15,46 @@ describe(tapOnce.name, () => {
 		});
 	});
 
+	it('should execute the function only once at the specified index', (done) => {
+		const tapFn = jest.fn();
+		const in$ = from([1, 2, 3, 4, 5]);
+		const out$ = in$.pipe(tapOnce(tapFn, 2));
+
+		out$.pipe(toArray()).subscribe((r) => {
+			expect(r).toEqual([1, 2, 3, 4, 5]);
+			expect(tapFn).toHaveBeenCalledTimes(1);
+			expect(tapFn).toHaveBeenCalledWith(3);
+			done();
+		});
+	});
+
 	it('should throw an error if tapIndex is negative', () => {
-		expect(() => tapOnce(() => {}, -1)).toThrow(
+		expect(() => tapOnce(() => void 0, -1)).toThrow(
 			'tapIndex must be a non-negative integer',
 		);
+	});
+
+	it('should not share state across multiple subscriptions', (done) => {
+		const tapFn = jest.fn();
+		const in$ = from([1, 2, 3, 4, 5]);
+		const out$ = in$.pipe(tapOnce(tapFn, 1));
+
+		// First subscription
+		out$.pipe(toArray()).subscribe((r1) => {
+			expect(r1).toEqual([1, 2, 3, 4, 5]);
+			expect(tapFn).toHaveBeenCalledTimes(1);
+			expect(tapFn).toHaveBeenCalledWith(2);
+
+			tapFn.mockClear();
+
+			// Second subscription - should also trigger the tap function
+			out$.pipe(toArray()).subscribe((r2) => {
+				expect(r2).toEqual([1, 2, 3, 4, 5]);
+				expect(tapFn).toHaveBeenCalledTimes(1);
+				expect(tapFn).toHaveBeenCalledWith(2);
+				done();
+			});
+		});
 	});
 });
 
@@ -71,6 +94,29 @@ describe(tapOnceOnFirstTruthy.name, () => {
 			expect(tapFn).toHaveBeenCalledTimes(1);
 			expect(tapFn).toHaveBeenCalledWith(1);
 			done();
+		});
+	});
+
+	it('should not share state across multiple subscriptions', (done) => {
+		const tapFn = jest.fn();
+		const in$ = from([0, null, false, 3, 4, 5]);
+		const out$ = in$.pipe(tapOnceOnFirstTruthy(tapFn));
+
+		// First subscription
+		out$.pipe(toArray()).subscribe((r1) => {
+			expect(r1).toEqual([0, null, false, 3, 4, 5]);
+			expect(tapFn).toHaveBeenCalledTimes(1);
+			expect(tapFn).toHaveBeenCalledWith(3);
+
+			tapFn.mockClear();
+
+			// Second subscription - should also trigger the tap function
+			out$.pipe(toArray()).subscribe((r2) => {
+				expect(r2).toEqual([0, null, false, 3, 4, 5]);
+				expect(tapFn).toHaveBeenCalledTimes(1);
+				expect(tapFn).toHaveBeenCalledWith(3);
+				done();
+			});
 		});
 	});
 });
